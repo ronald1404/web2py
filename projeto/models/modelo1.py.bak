95bba0012c650f65455069dc89bcee515aaaf85f
ronald <ronald.andrade.02@gmail.com>
2023-01-11 12:28:25 -0300

    first commit

---
+++ projeto/models/modelo1.py.bak
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+print (globals())
+print ('_'*80)
+lista1 = ['lista1','primeiro a ser executado']
+print (lista1)
---
+++ projeto/models/modelo2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+print ('_'*80)
+lista2 = ['lista2','segundo a ser executado']
+print (lista2)
+
+import calendar
+meses = [month for month in calendar.month_name]
+del meses[0]
+print (meses)
